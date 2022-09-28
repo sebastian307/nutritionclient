8733bb5d5ee15d45b3e5efb032b7f5184d815c97
sebastian307 <36278147+sebastian307@users.noreply.github.com>
2022-09-28 22:13:29 +0200

    Improved Selection for Live-search

--- src/components/food/AddStockModal.js
+++ src/components/food/AddStockModal.js
@@ -5,6 +5,8 @@ const AddFoodModal = ({ setModalOn, setChoice, entitySizeList }) => {
     const [sizeType, setSizeType] = useState(entitySizeList[0].entitySizeType);
     const [query, setQuery] = useState("");
     const [searchResults, setSearchResults] = useState({});
+    const [selectedFood, setSelectedFood] = useState("");
+    const [foodInput, setFoodInput] = useState("");
 
 
     const handleOKClick = async () => {
@@ -21,13 +23,30 @@ const AddFoodModal = ({ setModalOn, setChoice, entitySizeList }) => {
             .then(response => response.json());
 
         window.location.reload();
-        
+
         setChoice(true)
         setModalOn(false)
     }
     const handleCancelClick = () => {
         setChoice(false)
         setModalOn(false)
+    }
+
+
+    const selectFood = (food) => {
+        setSelectedFood(food);
+        setFoodInput(food.name);
+        console.log("Gewaehlte ID:"+food.id)
+    }
+
+    const foodInputChanged = (input) => {
+        setQuery(input);
+        setFoodInput(input);
+    }
+    const foodInputBlur = () => {
+        if (selectFood!=""){
+            setFoodInput(selectedFood.name);
+        }
     }
 
     useEffect(() => {
@@ -45,7 +64,6 @@ const AddFoodModal = ({ setModalOn, setChoice, entitySizeList }) => {
         }
     }, [query])
 
-
     return (
 
         <div className="   bg-zinc-600 bg-opacity-80 fixed inset-0 z-50   ">
@@ -54,7 +72,7 @@ const AddFoodModal = ({ setModalOn, setChoice, entitySizeList }) => {
 
                 <div className="flex-col justify-center  bg-white py-10 px-20 border-4 drop-shadow-2xl rounded-xl ">
 
-                    <div className="flex  text-lg  text-zinc-600   mb-10" >Add Food</div>
+                    <div className="flex  text-lg  text-zinc-600   mb-10" >Add Stock</div>
                     <form className="">
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
@@ -62,11 +80,22 @@ const AddFoodModal = ({ setModalOn, setChoice, entitySizeList }) => {
                             </label>
                             <input
                                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                id="username" type="text" placeholder="Start typing..." value={query} onChange={e => setQuery(e.target.value)}/>
+                                id="username" autoComplete="off" type="text" placeholder="Start typing..." value={foodInput} onChange={e => (foodInputChanged(e.target.value))}
+                                onBlur={foodInputBlur}/>
 
-                            {searchResults.map(result => (<div>
-                                {result.name}
-                            </div>))}
+                                <ul className="overflow-auto max-h-44 rounded w-full leading-tight text-center">
+                                    {searchResults.length ? searchResults.map(result => {
+                                        if (result.id!=selectedFood.id){
+                                        return <div onClick={() => selectFood(result)} className="p-5 mt-3 rounded-xl border-solid border-2 border-sky-500 text-sm font-bold" key={result.id}>
+                                            {result.name}
+                                        </div>;}
+
+                                        //Else:
+                                        return <div onClick={() => selectFood(result)} className="p-5 mt-3 rounded-xl border-solid border-2 border-sky-500 bg-gradient-to-r from-sky-500 to-cyan-500 text-white text-sm font-bold" key={result.id}>
+                                            {result.name}
+                                        </div>;
+                                    }) : ""}
+                                </ul>
 
                         </div>
                         <div className="mb-6">
@@ -78,7 +107,7 @@ const AddFoodModal = ({ setModalOn, setChoice, entitySizeList }) => {
                             </select>
                         </div>
                         <div className="flex items-center justify-between">
-                            <div className="flex">
+                             <div className="flex">
                                 <button onClick={handleOKClick} className=" rounded px-4 py-2 text-white  bg-green-400 ">Yes</button>
                                 <button onClick={handleCancelClick} className="rounded px-4 py-2 ml-4 text-white bg-blue-500 ">No</button>
                             </div>
